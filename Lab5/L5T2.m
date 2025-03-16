7f61f0ddb951f8fb47eea2d39e862369624e0e42
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2025-03-17 00:11:23 +0400

    update

--- Lab5/L5T2.m
+++ Lab5/L5T2.m
@@ -11,22 +11,18 @@ den = [1 4.5 15];     % Denominator: s³ + 4.5s² + 15
 % Create Transfer Function
 H = tf(num, den);
 
-% Display Transfer Function
-disp('Transfer Function:');
-disp(H);
+% Generate Bode Plot and get data
+[mag, phase, w] = bode(H);
+mag = squeeze(mag);
+phase = squeeze(phase);
+w = squeeze(w);
+mag_dB = 20*log10(mag);
+
+disp(table(w, mag_dB, phase, 'VariableNames', {'Frequency_rad_s', 'Magnitude_dB', 'Phase_deg'}));
 
 % Generate Bode Plot
-figure;
 bode(H);
 grid on;
-title('Bode Plot of RLC Circuit');
 
-% Get magnitude and phase values
-[mag, phase, w] = bode(H);
-
-% Convert magnitude to dB
-mag_db = 20*log10(squeeze(mag));
-
-% Display results in a table
-results_table = table(w, mag_db, squeeze(phase), 'VariableNames', {'Frequency_rad_s', 'Magnitude_dB', 'Phase_deg'});
-disp(results_table(1:10:end,:));  % Display every 10th row for brevity
+% Display transfer function
+disp(H);
